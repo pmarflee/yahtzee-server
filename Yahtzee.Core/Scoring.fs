99d8c82e2a5a389ce39f73e7909ef4358f4cb7f6
pmarflee <paul.marfleet@gmail.com>
2012-09-23 14:42:06 +0100

    Consolidated scoring of 3-of-a-kind, 4-of-a-kind, Full House, and Yahtzee combinations into a single function

--- Yahtzee.Core/Scoring.fs
+++ Yahtzee.Core/Scoring.fs
@@ -12,30 +12,28 @@ module Scoring =
 
     module private ScoringUtils =
 
-        let nOfAKind dice n =
-            dice 
-            |> Seq.groupBy id
-            |> Seq.filter (fun (_, dice) -> dice |> Seq.length >= n)
-            |> Seq.tryPick (fun (value, dice) -> Some(value, Seq.length dice))
-
-        let nOfAKindScorer dice n scorer =
-            match nOfAKind dice n with
-            | Some(_) -> scorer dice
-            | None -> 0
+        let ofAKindScorer dice category scorer =
+            match
+                match dice
+                  |> Seq.groupBy id
+                  |> Seq.map (fun (die, seq) -> die, Seq.length seq)
+                  |> Seq.sortBy (fun (die, count) -> -count)
+                  |> Seq.toList with
+                | (_, 3) :: tl when category = ThreeOfAKind -> true
+                | (_, 4) :: tl when category = ThreeOfAKind || category = FourOfAKind -> true
+                | (_, 5) :: tl when category = Yahtzee -> true
+                | (_, 3) :: (_, 2) :: tl when category = FullHouse -> true
+                | _ -> false 
+                with
+            | true -> scorer dice
+            | false -> 0
 
         let sumOfDice dice = List.sumBy int dice
-        let threeOfAKindScorer dice = nOfAKindScorer dice 3 sumOfDice
-        let fourOfAKindScorer dice = nOfAKindScorer dice 4 sumOfDice
-        let yahtzeeScorer dice = nOfAKindScorer dice 5 (fun _ -> int LowerSectionScore.Yahtzee)
-
-        let fullHouseScorer dice =
-            match dice
-              |> Seq.groupBy id
-              |> Seq.map (fun (_, seq) -> Seq.length seq)
-              |> Seq.sort
-              |> Seq.toList with
-            | [2; 3] -> int LowerSectionScore.FullHouse
-            | _ -> 0
+        let scoreYahtzee = (fun _ -> int LowerSectionScore.Yahtzee)
+        let threeOfAKindScorer dice = ofAKindScorer dice ThreeOfAKind sumOfDice
+        let fourOfAKindScorer dice = ofAKindScorer dice FourOfAKind sumOfDice
+        let fullHouseScorer dice = ofAKindScorer dice FullHouse (fun _ -> int LowerSectionScore.FullHouse)
+        let yahtzeeScorer dice = ofAKindScorer dice Yahtzee scoreYahtzee
 
         let straightScorer dice options score =
             let set = dice |> Set.ofList
@@ -48,13 +46,9 @@ module Scoring =
         let largeStraightScorerOptions = getStraightScorerOptions 5
 
         let smallStraightScorer dice = straightScorer dice smallStraightScorerOptions (int LowerSectionScore.SmallStraight)
-
         let largeStraightScorer dice = straightScorer dice largeStraightScorerOptions (int LowerSectionScore.LargeStraight)
-
         let chanceScorer dice = List.sumBy int dice
-
-        let upperCategoryScorer value dice =
-            List.sumBy (fun die -> if int die = value then int die else 0) dice
+        let upperCategoryScorer value dice = List.sumBy (fun die -> if int die = value then int die else 0) dice
 
         let scorers = 
             [ Ones, upperCategoryScorer 1;
@@ -76,9 +70,9 @@ module Scoring =
 
     let GetScorer category = Map.find category ScoringUtils.scorers
 
-    let IsYahtzee (dice : Die list) = (ScoringUtils.nOfAKind dice 5).IsSome
+    let IsYahtzee (dice : Die list) = ScoringUtils.yahtzeeScorer dice = int LowerSectionScore.Yahtzee
 
-    let NOfAKind dice = ScoringUtils.nOfAKindScorer dice 5 ScoringUtils.sumOfDice
+    let NOfAKind dice = ScoringUtils.ofAKindScorer dice Yahtzee ScoringUtils.sumOfDice
     
     let ScoreJoker turn =
         match turn.Category with
